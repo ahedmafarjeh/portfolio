1b38c7356cbf8ff9c0a9feb14e7d70fe616fa80f
ahedmafarjeh <ahedmafarjeh10@gmail.com>
2024-11-16 22:40:21 +0200

    project version 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,8 +36,22 @@ age_element = document.querySelector(".age span");
 curren_date = new Date().getTime();
 DB = new Date("11/28/1997").getTime();
 diff = curren_date - DB;
-days = Math.round(diff / (1000 * 3600 * 24))
-my_age = Math.floor(days / 365)
-
+days = Math.round(diff / (1000 * 3600 * 24));
+my_age = Math.floor(days / 365);
 age_element.innerHTML = my_age;
 
+// configure email js
+function sendMail(){
+  emailjs.send("service_slkd34a","template_oxuayhb",{
+    from_name: "ahmad",
+    email_id: "ahmad@gmail.com",
+    message: "test message",
+    });
+  // let params={
+  //   from_name: document.querySelector("#fname").value + document.querySelector("#lname").value,
+  //   email_id: document.querySelector("#email").value,
+  //   message: document.querySelector("#msg").value,
+  // };
+  // console.log(params.from_name + " " + params.email_id + " " + params.message)
+  // emailjs.send("service_slkd34a","template_oxuayhb",params).then(alert("Email Sent ☻"));
+}
